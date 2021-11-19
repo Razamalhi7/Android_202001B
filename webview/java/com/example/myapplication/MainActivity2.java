048b7a5c8491242abea3ffb2ba5a1d8815cb9c0d
Muhammad Adil Kamal <Malhiviki@gmail.com>
2021-11-19 10:58:48 +0500

    WebView

---
+++ webview/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        getSupportActionBar().hide();
+
+        webView=findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setAppCacheEnabled(true);
+
+       // webView.loadUrl("https://www.bbc.com/weather/1174872");
+webView.loadUrl("file:///android_asset/smartedu/index.html");
+    }
+}
