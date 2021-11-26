b45e9161ad5b411a73ff70ff1a929ff1151d0750
Muhammad Adil Kamal <Malhiviki@gmail.com>
2021-11-26 10:39:50 +0500

    Video Player

--- video view/java/com/example/myapplication/MainActivity.java
+++ video view/java/com/example/myapplication/MainActivity.java
@@ -3,51 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView videoView;
 
-    MediaPlayer mediaPlayer;
-    Uri uri=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    boolean isStop=true;
-    public void btn_play(View view)
-    {
-        if(uri!=null)
-        {
-            if(isStop)
-            {
-                mediaPlayer=MediaPlayer.create(this,uri);
-                isStop=false;
-            }
-            mediaPlayer.start();
 
-        }
-
-        else
-            Toast.makeText(this, "Audio file not found", Toast.LENGTH_SHORT).show();
+        videoView=findViewById(R.id.videoview);
+        MediaController mediaController=new MediaController(this);
+        videoView.setMediaController(mediaController);
 
 
-    }
-
-    public void btn_pause(View view)
-    {
-        mediaPlayer.pause();
-    }
-
-    public void btn_stop(View view)
-    {
-        mediaPlayer.stop();
-        isStop=true;
     }
 
     public void btn_browse(View view)
@@ -67,10 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         if(requestCode==101 && resultCode==RESULT_OK)
         {
-            uri=data.getData();
-            isStop=true;
+           Uri uri=data.getData();
+           videoView.setVideoURI(uri);
         }
 
 
     }
+
 }
