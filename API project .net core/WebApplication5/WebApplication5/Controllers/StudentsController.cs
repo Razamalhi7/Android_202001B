a06b75aa90b5148b240b992332fb1108f7048e27
Muhammad Adil Kamal <Malhiviki@gmail.com>
2022-01-03 10:59:46 +0500

    Api project .net core

---
+++ API project .net core/WebApplication5/WebApplication5/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApplication5.Model;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+        public StudentsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public Student GetStudents(Student student)
+        {
+
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+            return student;
+        }
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            return _context.Students.ToList();
+        }
+
+
+        [HttpGet("{id:int}")]
+        public object GetStudent(int id)
+        {
+            var student = _context.Students.FirstOrDefault(x=>x.StudentId==id);
+            if (student == null)
+                return new { StatusCode=404,Message="Record not found" };
+
+            return student;
+        }
+        [HttpDelete("{id:int}")]
+        public object DeleteStudent(int id)
+        {
+            var student = _context.Students.FirstOrDefault(x => x.StudentId == id);
+            if (student == null)
+                return new { StatusCode = 404, Message = "Record not found" };
+
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            return  new { StatusCode = 202, Message = $"Record deleted of id {id}" };
+        }
+    }
+}
---
+++ API project .net core/WebApplication5/WebApplication5/Model/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Model
+{
+    public class StudentDbContext :DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options): base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
